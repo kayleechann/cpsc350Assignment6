0f6fa4759bd5e8cc89ecbf6b35080555eb582603
root <kaychan@chapman.edu>
2020-12-20 03:46:35 +0000

    finished

--- main.cpp
+++ main.cpp
@@ -3,8 +3,6 @@
 
 int main(int argc, char **argv) {
   Sorting s;
-  // GenerateFile gf;
-  // gf.toFile();
   if(argc != 2){
     cout << "ERROR: not a valid number of command line argumnets! " << endl;
   }else if(argc == 2){
